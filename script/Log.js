1f1997626a10a2b8ed178bf65ae2abd40d6db46a
Azizdev03 <ad3005913@gmail.com>
2024-07-13 13:30:50 +0000

    Log.js

--- script/Log.js
+++ script/Log.js
@@ -13,10 +13,16 @@ module.exports = {
                         const memberCount = threadInfo.participantIDs.length;
  
                         // If the bot is added to the group
-                        api.sendMessage(`𝗖𝗢𝗡𝗡𝗘𝗖𝗧𝗘𝗗 𝗦𝗨𝗖𝗦𝗘𝗦𝗦\n[✦🆔] [${groupName}]\n━━━━━━━━━━━━━[${memberCount}]\n𝙰𝚎𝚜𝚝𝚑𝚎𝚛`, event.threadID);
+                        api.sendMessage(`𝗖𝗢𝗡𝗡𝗘𝗖𝗧𝗘𝗗 𝗦𝗨𝗖𝗦𝗘𝗦𝗦\n[✦🆔] [${groupName}]\n━━━━━━━━━━━━━[${memberCount}]\nAziz`, event.threadID);
                     } else {
                         // If any other participant is added to the group
-                        api.sendMessage(`𝖧𝖾𝗅𝗅𝗈 ${name} 𝗐𝖾𝗅𝖼𝗈𝗆𝖾 𝗍𝗈 𝗍𝗁𝖾 𝖦𝗋𝗈𝗎𝗉 𝖤𝗇𝗃𝗈𝗒 𝗒𝗈𝗎𝗋 𝖲𝗍𝖺𝗒 👋`, event.threadID);
+                        api.sendMessage(`Bonjour ${name} 🎉🌟, bienvenue dans notre groupe ! 🌟🎉
+
+Nous sommes ravis de t'accueillir parmi nous avec grand enthousiasme et chaleur. 🤗🌺 Ici, c'est un véritable petit cocon de bonne humeur, de partage et d'amitié, où chacun trouve sa place et peut s'épanouir. 🌈💫
+
+N'hésite pas à te joindre à nos discussions, à partager tes idées et à faire partie de notre belle communauté. 🤝💬 Ensemble, nous créerons des moments magiques et des souvenirs inoubliables. 🌟✨
+
+Que cette nouvelle aventure parmi nous soit remplie de joie, de complicité et d'échanges enrichissants. 🌻🌈 Bienvenue à toi, cher membre, dans notre famille virtuelle. 🥳🤗`, event.threadID);
                     }
                 } catch (error) {
                     console.error("Error:", error);
